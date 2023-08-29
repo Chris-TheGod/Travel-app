ecf35c472b41c0b48b8ec6e380e762cd37bd144a
Kristjan Lääne <xonproject@gmail.com>
2023-08-29 20:42:39 +0300

    displaying data from API

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,35 +1,43 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 const url = 'https://course-api.com/react-tours-project'
 export const Card = () => {
+  const [tourData, setTourData] = useState(null)
+
   useEffect(() => {
     const fetchData = async () => {
-      const result = await fetch(url)
-      result.json().then((data) => {
+      try {
+        const response = await fetch(url)
+        const data = await response.json()
+        setTourData(data)
         console.log(data)
-      })
+      } catch (error) {
+        console.error('Error fetching data')
+      }
     }
     fetchData()
   }, [])
 
   return (
     <div>
-      <img src='' alt='' />
-      <div>Best of Paris in 7 days</div>
-      <div>$1,995</div>
-      <div>
-        Paris is synonymous with the finest things that culture can offer — in
-        art, fashion, food, literature, and ideas. On this tour, your
-        Paris-savvy Rick Steves guide will immerse you in the very best of the
-        City of Light: the masterpiece-packed Louvre and Orsay museums,
-        resilient Notre-Dame Cathedral, exquisite Sainte-Chapelle, and
-        extravagant Palace of Versailles. You'll also enjoy guided neighborhood
-        walks through the city's historic heart as well as quieter moments to
-        slow down and savor the city's intimate cafés, colorful markets, and
-        joie de vivre. Join us for the Best of Paris in 7 Days!
-      </div>
-      <button>Show Less</button>
-      <button>Not interested</button>
+      {tourData ? (
+        <>
+          <div>Our Tours</div>
+          {tourData.map(({ image, name, id, info, price }) => (
+            <div key={id}>
+              {' '}
+              <img src={image} alt='' />
+              <div>{name}</div>
+              <div>{price}</div>
+              <div>{info}</div>
+              <button>Show Less</button>
+              <button>Not interested</button>
+            </div>
+          ))}
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   )
 }
